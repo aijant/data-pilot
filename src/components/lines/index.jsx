6236534f8dae12843020487fc858fc5058f074d6
Temirlan Isabekov <temirlan.i@jetbase.io>
2023-05-20 17:59:03 +0600

    finish back lines

--- src/components/lines/index.jsx
+++ src/components/lines/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import line1 from '../../assets/images/line-1.svg'
 import line2 from '../../assets/images/line-2.svg'
+import line3 from '../../assets/images/line-3.svg'
+import line4 from '../../assets/images/line-4.svg'
 import styles from './styles.module.scss'
 
 export const Lines = () => {
@@ -8,6 +10,9 @@ export const Lines = () => {
     <>
       <img className={styles.line1} src={line1} alt='' />
       <img className={styles.line2} src={line2} alt='' />
+      <img className={styles.line3} src={line3} alt='' />
+      <img className={styles.line4} src={line4} alt='' />
+      <img className={styles.line5} src={line4} alt='' />
     </>
   )
 }
