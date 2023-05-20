5c5e102eee06b9681c8cbea910cb366d26926666
Aizhan Tilek <aijanm01@gmail.com>
2023-05-20 17:34:15 +0600

    add: buy & footer block

--- src/pages/buy.jsx
+++ src/pages/buy.jsx
@@ -7,13 +7,13 @@ function Buy() {
             <p>Пожалуйста, введите свой адрес электронной почты,<br /> мы вышлем письмо с новым паролем:</p>
             
             <form>
-                <input type="text" placeholder='DataPilot  1.1' />
-                <input type="text" placeholder='наименование лицензии' />
-                <input type="text" placeholder='Описание лицензии' />
-                <input type="emai" placeholder='Email' />
-                <input type="number" placeholder='Стоимость' />
-                <button>купить</button>
-                <button>отмена</button>
+                <div><input type="text" placeholder='DataPilot  1.1' /></div>
+                <div><input type="text" placeholder='наименование лицензии' /></div>
+                <div> <input type="text" placeholder='Описание лицензии' /></div>
+                <div><input type="emai" placeholder='Email' /></div>
+                <div><input type="number" placeholder='Стоимость' /></div>
+                <div><button className='buy'>купить</button></div>
+                <div><button className='cancel'>отмена</button></div>
             </form>
         </div>
     );
---
+++ src/pages/footer.jsx
@@ -0,0 +1,22 @@
+import './footer.scss'
+import logo from '../assets/images/logo.svg'
+
+function Footer() {
+    return (
+        <div className="footer">
+            <div className='line'></div>
+            <div><img src={logo} alt='logo' /></div>
+            <div>
+                <p>
+                    Al Mamoura A Building,<br />
+                    Abu Dhabi, United Arab Emirates<br />
+                    P.O. Box 45005<br />
+                    Telephone: +971 2413 1993<br />
+                    Fax: +971 2413 2023
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
